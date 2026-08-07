8e084c97999737cc55e93466e2b2e71252bf510a
piumini udagedara <piuminiu@gmail.com>
2026-08-08 03:41:07 +0530

    fix

--- backend/dist/prisma.config.js
+++ backend/dist/prisma.config.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./scripts/load-env");
 const config_1 = require("prisma/config");
-const databaseUrl = process.env.DATABASE_URL ??
-    'postgresql://build:build@localhost:5432/build';
+const databaseUrl = process.env.DATABASE_URL ?? 'postgresql://build:build@localhost:5432/build';
 exports.default = (0, config_1.defineConfig)({
     schema: 'prisma/schema.prisma',
     migrations: {
